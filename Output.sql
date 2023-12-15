01cbdb0898a523b288c4000310df386bddf22ebc
JP852 <142391590+JP852@users.noreply.github.com>
2023-12-15 16:16:12 +0000

    Create Output.sql

---
+++ Output.sql
@@ -0,0 +1,34 @@
+WITH CTE AS(
+SELECT
+SUM(VALUE) AS SUM_OF_VALUE
+,IFF(
+LENGTH(transaction_code) = 15,LEFT(transaction_code,3),LEFT(transaction_code,2)) AS BANK
+//,DATE(transaction_date,'dd/MM/yyyy hh24:mi:ss') as MONTH_2
+,MONTHNAME(DATE(transaction_date,'dd/MM/yyyy hh24:mi:ss')) as MONTH
+,RANK() OVER(PARTITION BY MONTHNAME(DATE(transaction_date,'dd/MM/yyyy hh24:mi:ss')) ORDER BY SUM(value) DESC)  AS RANK
+FROM  
+PD2023_WK01
+GROUP BY BANK,MONTH
+)
+
+,AVG_RANK AS(
+SELECT BANK
+,AVG(RANK) AS AVG_RANK_ACROSS_ALL_MONTHS
+FROM CTE 
+GROUP BY BANK
+)
+,AVG_RANK_VALUE AS(
+SELECT RANK
+,AVG(SUM_OF_VALUE) AS Avg_Transaction_per_Rank
+FROM CTE
+GROUP BY RANK
+)
+SELECT 
+C.* 
+,AVG_RANK_ACROSS_ALL_MONTHS AS AVG_RANK_PER_BANK
+,Avg_Transaction_per_Rank AS Avg_Transaction_value_per_Rank
+FROM CTE AS C
+INNER JOIN AVG_RANK AS AR
+ON C.BANK = AR.BANK
+INNER JOIN AVG_RANK_VALUE AS AVR
+ON AVR.RANK = C.RANK
