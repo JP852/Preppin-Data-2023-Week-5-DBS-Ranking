8b3659e9f0e505abf71ef3d3ce0060c3eb802767
JP852 <142391590+JP852@users.noreply.github.com>
2024-04-30 09:12:11 +0100

    Update Output.sql

--- Output.sql
+++ Output.sql
@@ -1,3 +1,13 @@
+
+-- Create the bank code by splitting out off the letters from the Transaction code, call this field 'Bank'
+-- Change transaction date to the just be the month of the transaction
+-- Total up the transaction values so you have one row for each bank and month combination
+-- Rank each bank for their value of transactions each month against the other banks. 1st is the highest value of transactions, 3rd the lowest. 
+-- Without losing all of the other data fields, find:
+--     - The average rank a bank has across all of the months, call this field 'Avg Rank per Bank'
+--     - The average transaction value per rank, call this field 'Avg Transaction Value per Rank'
+
+
 WITH CTE AS(
 SELECT
 SUM(VALUE) AS SUM_OF_VALUE
